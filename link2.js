2b2f7bc712396ec77890dc31d2d4ff73b458068a
sanithyamk <168641728+sanithyamk@users.noreply.github.com>
2025-05-20 21:44:28 +0530

    Add files via upload

---
+++ link2.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",function()
+{
+this.setTimeout(
+    function open(event){
+document.querySelector(".dis").style.display="block";
+    },
+)
+});
+
+document.querySelector("#close").addEventListener("click",function()
+
+{
+document.querySelector(".dis").style.display="none";
+});
